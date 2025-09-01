eea1de18497f4163367c4bf81d6ca5ee74ed41d8
sindhupallavi14 <sindhuramanadula14@gmail.com>
2025-09-01 17:40:03 +0530

    updated

--- src/Components/Hero/findBySpeciality.jsx
+++ src/Components/Hero/findBySpeciality.jsx
@@ -26,7 +26,8 @@ function FindBySpeciality() {
           {specialties.map((spec, idx) => (
             <Link
               key={idx}
-              to={`/all-doctors/${spec.name}`} 
+              to={`/all-doctors/${spec.name.toLowerCase()}`}
+
               className="flex flex-col items-center min-w-[90px] lg:min-w-[150px] snap-start"
             >
               <img
